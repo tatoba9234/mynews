e35fce7c6f1a095685afea5db835012c7c08c3be
”Koyo” <tatoba9234@gmail.com>
2020-08-26 08:43:35 +0000

    GET/POST method

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::post('news/create', 'Admin\NewsController@create');
 });
 
 
@@ -27,9 +28,11 @@ Route::get('XXX', 'Admin\AAAController@bbb');
 
 //課題4のソースコード
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
+  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::post('profile/create', 'Admin\ProfileController@create');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 
