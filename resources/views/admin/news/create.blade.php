f88f45dfe702d472e13bcde1897bcedc03535ba7
”Koyo” <tatoba9234@gmail.com>
2020-09-11 05:45:49 +0000

    heroku uodate

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -14,7 +14,7 @@
  <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>ニュースを作成しよう</h2>
                 <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
 
                      @if (count($errors) > 0)
